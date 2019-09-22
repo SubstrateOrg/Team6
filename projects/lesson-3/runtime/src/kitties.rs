dad1ce9e222dfdc4a7fb5f1731c9fa4609581d72
andyyi <yijianan1426@gmail.com>
2019-09-22 22:18:23 +0800

    第三课作业

--- projects/lesson-3/runtime/src/kitties.rs
+++ projects/lesson-3/runtime/src/kitties.rs
@@ -4,24 +4,51 @@ use runtime_io::blake2_128;
 use system::ensure_signed;
 
 /*
-V2需求 繁殖⼩小猫
-选择两只现有的猫作为⽗父⺟母
-⼩小猫必须继承⽗父⺟母的基因
-同样的⽗父⺟母⽣生出来的⼩小猫不不能相同
+V1需求
+	链上存储加密猫数据
+	每个用户可以拥有零到多只猫
+	遍历所有加密猫
+	每只猫只有⼀一个主⼈
+	遍历⽤户拥有的所有猫
+	每只猫都有⾃己的dna，为128bit的数据
+	设计如何生成dna (伪代码算法)
+
+V2需求 
+	繁殖小⼩猫,选择两只现有的猫作为⽗母
+	⼩小猫必须继承⽗母的基因
+	同样的⽗母⽣生出来的⼩小猫不能相同
 */
 
 pub trait Trait: system::Trait {
 }
 
-#[derive(Encode, Decode, Default)]
-pub struct Kitty(pub [u8; 16]);
+#[derive(Encode, Decode, Clone, Default)]
+pub struct Kitty<T> where T::Trait {
+	dna: [u8, 16],
+	owner: T::AccountId,
+	price: u32,
+};
 
 decl_storage! {
-	trait Store for Module<T: Trait> as Kitties {
+	trait Store for Module<T: Trait> as kitties {
 		/// Stores all the kitties, key is the kitty id / index
 		pub Kitties get(kitty): map u32 => Kitty;
 		/// Stores the total number of kitties. i.e. the next kitty index
 		pub KittiesCount get(kitties_count): u32;
+		pub OwnerKitties get(owner_kitties): map T::AccountId => Option<Vec<Kitty>>;
+	}
+}
+
+impl <T> OwnerKitties<T> where T::Trait {
+	fn add_kitty(owner: T::AccountId, kitty: Kitty) {
+		let mut kitties;
+		if let Some(one) = Self:get(owner.clone()) {
+			kitties = one;
+		} else if {
+			kitties = Vec::new();
+		}
+		kitties.push(kitty);
+		Self::insert(owner, kitties);
 	}
 }
 
@@ -32,27 +59,33 @@ decl_module! {
 			let sender = ensure_signed(origin)?;
 			let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 			let dna = payload.using_encoded(blake2_128);
-			Self::do_create(dna)
+			Self::do_create(sender, dna)
 		}
 		/// Breed a new kitty from mother and farther
 		pub fn breed(origin, mother: u32, farther, u32) -> Result {
 			let sender = ensure_signed(origin)?;
 			let payload = (mother + farther + <system::Module<T>>::random_seed());
 			let dna = payload.using_encoded(blake2_128);
-			Self::do_create(dna)
+			Self::do_create(sender, dna)
 		}
+
 	}
 }
 
 impl<T: Trait> Module<T> {
-	fn do_create(dna: [u8; 16]) -> Result {
+	fn do_create(sender: T::AccountId, dna: [u8; 16]) -> Result {
 			let count = Self::kitties_count();
 			if count == u32::max_value() {
 				return Err("Kitties count overflow");
 			}
-			let kitty = Kitty(dna);
+			let kitty = Kitty{
+				dna: dna,
+				owner: sender.clone(),
+				price: 1,
+			};
 			Kitties::insert(count, kitty);
 			KittiesCount::put(count + 1);
+			<OwnerKitties<T>>::add_kitty(sender.clone(), kitty);
 			Ok(())
 	}
 }
