5e85f83915b81a885a5b582507861bedd8432ba1
andyyi <yijianan1426@gmail.com>
2019-09-24 15:34:31 +0800

    update

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -33,25 +33,7 @@ decl_module! {
 			let sender = ensure_signed(origin)?;
 
 			// 作业：重构create方法，避免重复代码
-
-			let kitty_id = Self::kitties_count();
-			if kitty_id == T::KittyIndex::max_value() {
-				return Err("Kitties count overflow");
-			}
-
-			// Generate a random 128bit value
-			let payload = (<system::Module<T>>::random_seed(), &sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
-			let dna = payload.using_encoded(blake2_128);
-
-			// Create and store kitty
-			let kitty = Kitty(dna);
-			<Kitties<T>>::insert(kitty_id, kitty);
-			<KittiesCount<T>>::put(kitty_id + 1.into());
-
-			// Store the ownership information
-			let user_kitties_id = Self::owned_kitties_count(&sender);
-			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
-			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+			Self::do_create(&sender)?;
 		}
 
 		/// Breed kitties
@@ -69,10 +51,41 @@ fn combine_dna(dna1: u8, dna2: u8, selector: u8) -> u8 {
 	// selector.map_bits(|bit, index| if (bit == 1) { dna1 & (1 << index) } else { dna2 & (1 << index) })
 	// 注意 map_bits这个方法不存在。只要能达到同样效果，不局限算法
 	// 测试数据：dna1 = 0b11110000, dna2 = 0b11001100, selector = 0b10101010, 返回值 0b11100100
-	return dna1;
+	let dna: u8 = 0;
+	for i in 1..7 {
+		if (&selector % (2**i) == 0.into()) {
+			if (&dna2 % (2**i) == 1.into())
+				dna += 2**i;
+		} else {
+			if (&dna1 % (2**i) == 1.into())
+				dna += 2**i;
+		}
+	}
+	return dna;
 }
 
 impl<T: Trait> Module<T> {
+	fn do_create(sender: &T::AccountId) -> Result {
+		let kitty_id = Self::kitties_count();
+		if kitty_id == T::KittyIndex::max_value() {
+			return Err("Kitties count overflow");
+		}
+
+		// Generate a random 128bit value
+		let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
+		let dna = payload.using_encoded(blake2_128);
+
+		// Create and store kitty
+		let kitty = Kitty(dna);
+		<Kitties<T>>::insert(kitty_id, kitty);
+		<KittiesCount<T>>::put(kitty_id + 1.into());
+
+		// Store the ownership information
+		let user_kitties_id = Self::owned_kitties_count(sender);
+		<OwnedKitties<T>>::insert((sender, user_kitties_id), kitty_id);
+		<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1.into());
+		Ok(())
+	}
 	fn random_value(sender: &T::AccountId) -> [u8; 16] {
 		let payload = (<system::Module<T>>::random_seed(), sender, <system::Module<T>>::extrinsic_index(), <system::Module<T>>::block_number());
 		payload.using_encoded(blake2_128)
