1d408a2975b8e6491ac578d336938678746755b8
lovingzizi <turingjohn2015@gmail.com>
2019-09-26 16:13:25 +0800

    home work

--- projects/lesson-4/runtime/src/kitties.rs
+++ projects/lesson-4/runtime/src/kitties.rs
@@ -121,6 +121,82 @@ impl<T: Trait> Module<T> {
 
 		Self::insert_kitty(sender, kitty_id, Kitty(new_dna));
 
+		
+
 		Ok(())
 	}
+	 fn transfer(origin, to: T::AccountId, kitty_id: T::Hash) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let owner = Self::owner_of(kitty_id).ok_or("No owner for this kitty")?;
+            ensure!(owner == sender, "You do not own this kitty");
+
+            Self::transfer_from(sender, to, kitty_id)?;
+
+            Ok(())
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    fn mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        let owned_kitty_count = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count = owned_kitty_count.checked_add(1)
+            .ok_or("Overflow adding a new kitty to account balance")?;
+
+        let all_kitties_count = Self::all_kitties_count();
+
+        let new_all_kitties_count = all_kitties_count.checked_add(1)
+            .ok_or("Overflow adding a new kitty to total supply")?;
+
+        <Kitties<T>>::insert(kitty_id, new_kitty);
+        <KittyOwner<T>>::insert(kitty_id, &to);
+
+        <AllKittiesArray<T>>::insert(all_kitties_count, kitty_id);
+        <AllKittiesCount<T>>::put(new_all_kitties_count);
+        <AllKittiesIndex<T>>::insert(kitty_id, all_kitties_count);
+
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
+
+        Ok(())
+    }
+
+    fn transfer_from(from: T::AccountId, to: T::AccountId, kitty_id: T::Hash) -> Result {
+        let owner = Self::owner_of(kitty_id).ok_or("No owner for this kitty")?;
+
+        ensure!(owner == from, "'from' account does not own this kitty");
+
+        let owned_kitty_count_from = Self::owned_kitty_count(&from);
+        let owned_kitty_count_to = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count_to = owned_kitty_count_to.checked_add(1)
+            .ok_or("Transfer causes overflow of 'to' kitty balance")?;
+
+        let new_owned_kitty_count_from = owned_kitty_count_from.checked_sub(1)
+            .ok_or("Transfer causes underflow of 'from' kitty balance")?;
+
+        let kitty_index = <OwnedKittiesIndex<T>>::get(kitty_id);
+        if kitty_index != new_owned_kitty_count_from {
+            let last_kitty_id = <OwnedKittiesArray<T>>::get((from.clone(), new_owned_kitty_count_from));
+            <OwnedKittiesArray<T>>::insert((from.clone(), kitty_index), last_kitty_id);
+            <OwnedKittiesIndex<T>>::insert(last_kitty_id, kitty_index);
+        }
+
+        <KittyOwner<T>>::insert(&kitty_id, &to);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count_to);
+
+        <OwnedKittiesArray<T>>::remove((from.clone(), new_owned_kitty_count_from));
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count_to), kitty_id);
+
+        <OwnedKittiesCount<T>>::insert(&from, new_owned_kitty_count_from);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count_to);
+
+        Self::deposit_event(RawEvent::Transferred(from, to, kitty_id));
 }
